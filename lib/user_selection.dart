cccaadddbe570d0a24246831d1f817855f6f244c
fwdAndroid <fwdkaleem@gmail.com>
2022-12-13 19:27:11 +0500

    Signup

--- lib/user_selection.dart
+++ lib/user_selection.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mylittlebakery/buyer/buyer_section.dart';
+import 'package:mylittlebakery/buyer/buyerAuth/buyer_section_signin.dart';
 import 'package:mylittlebakery/widgets/utils.dart';
 
 class UserSelection extends StatelessWidget {
@@ -108,10 +108,8 @@ class UserSelection extends StatelessWidget {
                 ),
                 MaterialButton(
                   onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (builder) => BuyerSection()));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (builder) => BuyerSignIn()));
                   },
                   child: Container(
                     // group211NpC (2:926)
