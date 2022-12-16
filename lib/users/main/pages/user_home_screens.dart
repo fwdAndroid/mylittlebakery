b6f00f2c562002e22dbb926d591cdc0d2b1dccc1
fwdAndroid <fwdkaleem@gmail.com>
2022-12-17 01:23:41 +0500

    Home_Screen_UI

--- lib/users/main/pages/user_home_screens.dart
+++ lib/users/main/pages/user_home_screens.dart
@@ -1,5 +1,11 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:mylittlebakery/widgets/utils.dart';
 
 class UserHomeScreen extends StatefulWidget {
   const UserHomeScreen({super.key});
@@ -11,6 +17,231 @@ class UserHomeScreen extends StatefulWidget {
 class _UserHomeScreenState extends State<UserHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("Users")
+                .doc(FirebaseAuth.instance.currentUser!.uid)
+                .snapshots(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                return new CircularProgressIndicator();
+              }
+              var document = snapshot.data;
+              return CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(document['photoURL']));
+            },
+          ),
+          elevation: 3,
+          centerTitle: true,
+          title: Padding(
+            padding: EdgeInsets.all(9),
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("Users")
+                  .doc(FirebaseAuth.instance.currentUser!.uid)
+                  .snapshots(),
+              builder: (context, AsyncSnapshot snapshot) {
+                if (!snapshot.hasData) {
+                  return new CircularProgressIndicator();
+                }
+                var document = snapshot.data;
+                return Text(
+                  (document['name']),
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                );
+              },
+            ),
+          ),
+          backgroundColor: Colors.white,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications_outlined,
+                  color: Colors.black,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ))
+          ],
+        ),
+        body: Column(children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+            decoration: BoxDecoration(
+              color: Color(0xffffffff),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x14000000),
+                  offset: Offset(0, 1),
+                  blurRadius: 7,
+                ),
+              ],
+            ),
+            child: Opacity(
+              opacity: 0.5,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+
+                  // searchitemug8 (1:86)
+                  hintText: 'Search item',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                  hintStyle: SafeGoogleFont(
+                    'Rubik',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    height: 1.185,
+                    letterSpacing: -0.2399999946,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+            decoration: BoxDecoration(
+              color: Color(0xffffffff),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x14000000),
+                  offset: Offset(0, 1),
+                  blurRadius: 7,
+                ),
+              ],
+            ),
+            child: Opacity(
+              opacity: 0.5,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  // searchitemug8 (1:86)
+                  hintText: 'Search nearby bakery',
+                  prefixIcon: Icon(
+                    Icons.location_pin,
+                    color: Colors.grey,
+                  ),
+                  hintStyle: SafeGoogleFont(
+                    'Rubik',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    height: 1.185,
+                    letterSpacing: -0.2399999946,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 1.5,
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 14 / 10,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10),
+                itemCount: 23,
+                itemBuilder: (BuildContext ctx, index) {
+                  return Container(
+                    width: 175,
+                    height: 260,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Image.asset("assets/coffee-shop-rafiki-jUx.png"),
+                        Positioned(
+                          top: 15,
+                          left: 25,
+                          child: Container(
+                            height: 25,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                color: Color(0xffFECEC1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.red,
+                                ),
+                                Text(
+                                  "4.0",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 21,
+                          top: 90,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(color: Colors.white),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Column(
+                                      children: [
+                                        SizedBox(
+                                          child: Text(
+                                            'Item Name',
+                                            style: SafeGoogleFont(
+                                              'Rubik',
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w600,
+                                              color: Color(0xff000000),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          child: Text(
+                                            'Category',
+                                            style: SafeGoogleFont(
+                                              'Rubik',
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w400,
+                                              color: Color(0xff000000),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: Icon(
+                                          Icons.favorite_outline,
+                                          color: Colors.pink,
+                                        ))
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+          ),
+        ]));
   }
 }
