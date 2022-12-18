2d7363b2b1dff15527e31968c9fa7afe2fe902a8
fwdAndroid <fwdkaleem@gmail.com>
2022-12-18 16:00:01 +0500

    Order Payment Pag UI Completed

--- lib/users/pages/orders/completed_orders_screen.dart
+++ lib/users/pages/orders/completed_orders_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mylittlebakery/users/pages/orders/share_experience.dart';
 import 'package:mylittlebakery/widgets/utils.dart';
 
 class OrdersCompleted extends StatefulWidget {
@@ -16,8 +17,58 @@ class _OrdersCompletedState extends State<OrdersCompleted> {
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
     return Scaffold(
+        appBar: AppBar(
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset("assets/back.png"),
+            ),
+          ),
+          elevation: 3,
+          centerTitle: true,
+          title: Text(
+            "Payment",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        backgroundColor: Colors.white,
         body: Column(
-      children: [],
-    ));
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/Hotel Booking-pana 1.png"),
+            Text(
+              "Congratulation",
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Your order has been completed",
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              height: 120,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (builder) => ShareExperience()));
+              },
+              child: Text(
+                "OK",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+              style: ElevatedButton.styleFrom(
+                  primary: Color(0xffFECEC1),
+                  fixedSize: Size(306, 59),
+                  shape: StadiumBorder()),
+            )
+          ],
+        ));
   }
 }
---
+++ lib/users/pages/orders/share_experience.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ShareExperience extends StatefulWidget {
+  const ShareExperience({super.key});
+
+  @override
+  State<ShareExperience> createState() => _ShareExperienceState();
+}
+
+class _ShareExperienceState extends State<ShareExperience> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
