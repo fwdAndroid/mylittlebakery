139e7d1dc70ad7f0872f3236005ee0daa15da8fa
fwdAndroid <fwdkaleem@gmail.com>
2022-12-20 11:29:46 +0500

    Redesign UI

--- lib/seller/buyerAuth/buyer_section_signin.dart
+++ lib/seller/buyerAuth/buyer_section_signin.dart
@@ -6,6 +6,7 @@ import 'package:mylittlebakery/seller/buyerAuth/buyer_section_signup.dart';
 import 'package:mylittlebakery/seller/main_section/mainscreen.dart';
 import 'package:mylittlebakery/database/Firebase_auth_data.dart';
 import 'package:mylittlebakery/widgets/snak.dart';
+import 'package:mylittlebakery/widgets/text_form_field.dart';
 import 'package:mylittlebakery/widgets/utils.dart';
 
 class SellerSignIn extends StatefulWidget {
@@ -83,74 +84,22 @@ class _SellerSignInState extends State<SellerSignIn> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Container(
-                        // autogroupwmgqS8L (55KsCsm8J9YQmahsyJWmGQ)
-                        margin: EdgeInsets.fromLTRB(
-                            0 * fem, 0 * fem, 7 * fem, 17 * fem),
-
-                        width: 303 * fem,
-                        decoration: BoxDecoration(
-                          color: Color(0xffffffff),
-                          borderRadius: BorderRadius.circular(10 * fem),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Color(0x19000000),
-                              offset: Offset(0 * fem, 0 * fem),
-                              blurRadius: 5 * fem,
-                            ),
-                          ],
-                        ),
-                        child: TextField(
-                          controller: emailController,
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Email',
-                            hintStyle: SafeGoogleFont(
-                              'Rubik',
-                              fontSize: 16 * ffem,
-                              fontWeight: FontWeight.w500,
-                              height: 1.185 * ffem / fem,
-                              letterSpacing: 0.32 * fem,
-                              color: Color(0x7f000000),
-                            ),
-                          ),
-                        ),
+                      TextFormInputField(
+                        textInputType: TextInputType.emailAddress,
+                        hintText: 'Email',
+                        controller: emailController,
                       ),
-                      Container(
-                        // autogroupwmgqS8L (55KsCsm8J9YQmahsyJWmGQ)
-                        margin: EdgeInsets.fromLTRB(
-                            0 * fem, 0 * fem, 7 * fem, 17 * fem),
-
-                        width: 303 * fem,
-                        decoration: BoxDecoration(
-                          color: Color(0xffffffff),
-                          borderRadius: BorderRadius.circular(10 * fem),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Color(0x19000000),
-                              offset: Offset(0 * fem, 0 * fem),
-                              blurRadius: 5 * fem,
-                            ),
-                          ],
-                        ),
-                        child: TextField(
-                          obscureText: true,
-                          controller: passController,
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Password',
-                            hintStyle: SafeGoogleFont(
-                              'Rubik',
-                              fontSize: 16 * ffem,
-                              fontWeight: FontWeight.w500,
-                              height: 1.185 * ffem / fem,
-                              letterSpacing: 0.32 * fem,
-                              color: Color(0x7f000000),
-                            ),
-                          ),
-                        ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormInputField(
+                        textInputType: TextInputType.visiblePassword,
+                        hintText: 'Password',
+                        isPass: true,
+                        controller: passController,
+                      ),
+                      SizedBox(
+                        height: 5,
                       ),
                       Container(
                         // forgotpasswordGFn (1:140)
@@ -174,57 +123,52 @@ class _SellerSignInState extends State<SellerSignIn> {
                 InkWell(
                   onTap: loginUser,
                   child: Container(
-                    // group1000003020yAC (1:283)
-                    margin: EdgeInsets.fromLTRB(
-                        40 * fem, 0 * fem, 40 * fem, 25.25 * fem),
-                    height: 58.75 * fem,
+                    // group1000003251tHA (1:284)
+                    width: 333,
+                    height: 58,
                     decoration: BoxDecoration(
+                      color: Color(0xfffecec1),
                       borderRadius: BorderRadius.circular(40 * fem),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x14000000),
+                          offset: Offset(20 * fem, 20 * fem),
+                          blurRadius: 25 * fem,
+                        ),
+                      ],
                     ),
-                    child: Container(
-                      // group1000003251tHA (1:284)
-                      width: double.infinity,
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Color(0xfffecec1),
-                        borderRadius: BorderRadius.circular(40 * fem),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0x14000000),
-                            offset: Offset(20 * fem, 20 * fem),
-                            blurRadius: 25 * fem,
-                          ),
-                        ],
-                      ),
-                      child: Center(
-                        child: _isLoading
-                            ? Center(
-                                child: CircularProgressIndicator(),
-                              )
-                            : Text(
-                                'Sign In',
-                                textAlign: TextAlign.center,
-                                style: SafeGoogleFont(
-                                  'Rubik',
-                                  fontSize: 18 * ffem,
-                                  fontWeight: FontWeight.w500,
-                                  height: 1.185 * ffem / fem,
-                                  color: Color(0xff000000),
-                                ),
+                    child: Center(
+                      child: _isLoading
+                          ? Center(
+                              child: CircularProgressIndicator(),
+                            )
+                          : Text(
+                              'Sign In',
+                              textAlign: TextAlign.center,
+                              style: SafeGoogleFont(
+                                'Rubik',
+                                fontSize: 18 * ffem,
+                                fontWeight: FontWeight.w500,
+                                height: 1.185 * ffem / fem,
+                                color: Color(0xff000000),
                               ),
-                      ),
+                            ),
                     ),
                   ),
                 ),
+                SizedBox(
+                  height: 10,
+                ),
                 InkWell(
                   onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (builder) => SellerSignUp()));
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (builder) => SellerSignUp()));
                   },
                   child: Container(
                     // donthaveanaccountsignupjYg (1:149)
-                    margin: EdgeInsets.fromLTRB(
-                        0 * fem, 0 * fem, 1 * fem, 61 * fem),
+
                     child: RichText(
                       textAlign: TextAlign.center,
                       text: TextSpan(
@@ -275,54 +219,51 @@ class _SellerSignInState extends State<SellerSignIn> {
                     ),
                   ),
                 ),
+                SizedBox(
+                  height: 10,
+                ),
                 Center(
-                  child: Container(
-                    // group1000003289xCU (1:150)
-                    margin: EdgeInsets.fromLTRB(
-                        3 * fem, 0 * fem, 4 * fem, 47 * fem),
-                    width: double.infinity,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          // line1H8G (1:152)
-                          margin: EdgeInsets.fromLTRB(
-                              50 * fem, 1 * fem, 8.5 * fem, 0 * fem),
-                          width: 83 * fem,
-                          height: 2 * fem,
-                          decoration: BoxDecoration(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        // line1H8G (1:152)
+                        margin: EdgeInsets.fromLTRB(
+                            50 * fem, 1 * fem, 8.5 * fem, 0 * fem),
+                        width: 83 * fem,
+                        height: 2 * fem,
+                        decoration: BoxDecoration(
+                          color: Color(0xff000000),
+                        ),
+                      ),
+                      Container(
+                        // orcontinuewithymn (1:151)
+                        margin: EdgeInsets.fromLTRB(
+                            0 * fem, 0 * fem, 12.5 * fem, 0 * fem),
+                        child: Text(
+                          'Or Continue with',
+                          textAlign: TextAlign.center,
+                          style: SafeGoogleFont(
+                            'Rubik',
+                            fontSize: 14 * ffem,
+                            fontWeight: FontWeight.w500,
+                            height: 1.185 * ffem / fem,
+                            letterSpacing: 0.28 * fem,
                             color: Color(0xff000000),
                           ),
                         ),
-                        Container(
-                          // orcontinuewithymn (1:151)
-                          margin: EdgeInsets.fromLTRB(
-                              0 * fem, 0 * fem, 12.5 * fem, 0 * fem),
-                          child: Text(
-                            'Or Continue with',
-                            textAlign: TextAlign.center,
-                            style: SafeGoogleFont(
-                              'Rubik',
-                              fontSize: 14 * ffem,
-                              fontWeight: FontWeight.w500,
-                              height: 1.185 * ffem / fem,
-                              letterSpacing: 0.28 * fem,
-                              color: Color(0xff000000),
-                            ),
-                          ),
+                      ),
+                      Container(
+                        // line25K2 (1:153)
+                        margin: EdgeInsets.fromLTRB(
+                            0 * fem, 7 * fem, 0 * fem, 0 * fem),
+                        width: 80 * fem,
+                        height: 2 * fem,
+                        decoration: BoxDecoration(
+                          color: Color(0xff000000),
                         ),
-                        Container(
-                          // line25K2 (1:153)
-                          margin: EdgeInsets.fromLTRB(
-                              0 * fem, 7 * fem, 0 * fem, 0 * fem),
-                          width: 80 * fem,
-                          height: 2 * fem,
-                          decoration: BoxDecoration(
-                            color: Color(0xff000000),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ],
@@ -350,8 +291,8 @@ class _SellerSignInState extends State<SellerSignIn> {
       Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-              builder: (builder) => SellerMainScreen(),
-                  ));
+            builder: (builder) => SellerMainScreen(),
+          ));
     } else {
       showSnakBar(rse, context);
     }
---
+++ lib/widgets/text_form_field.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:mylittlebakery/widgets/utils.dart';
+
+class TextFormInputField extends StatelessWidget {
+  final TextEditingController controller;
+  final bool isPass;
+  final String hintText;
+  final TextInputType textInputType;
+
+  const TextFormInputField(
+      {Key? key,
+      required this.controller,
+      this.isPass = false,
+      required this.hintText,
+      required this.textInputType})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //  Container(
+    //                     // autogroupwmgqS8L (55KsCsm8J9YQmahsyJWmGQ)
+    //                     margin: EdgeInsets.fromLTRB(
+    //                         0 * fem, 0 * fem, 7 * fem, 17 * fem),
+
+    //                     width: 303 * fem,
+    //                     decoration: BoxDecoration(
+    //                       color: Color(0xffffffff),
+    //                       borderRadius: BorderRadius.circular(10 * fem),
+    // boxShadow: [
+    //   BoxShadow(
+    //     color: Color(0x19000000),
+    //     offset: Offset(0 * fem, 0 * fem),
+    //     blurRadius: 5 * fem,
+    //   ),
+    // ],
+    //                     ),
+    //                     child: TextField(
+    //                       controller: emailController,
+    //                       textAlign: TextAlign.center,
+    //                       decoration: InputDecoration(
+    //                         border: InputBorder.none,
+    //                         hintText: 'Email',
+    //                         hintStyle: SafeGoogleFont(
+    //                           'Rubik',
+    //                           fontSize: 16 * ffem,
+    //                           fontWeight: FontWeight.w500,
+    //                           height: 1.185 * ffem / fem,
+    //                           letterSpacing: 0.32 * fem,
+    //                           color: Color(0x7f000000),
+    //                         ),
+    //                       ),
+    //                     ),
+    //                   ),
+    final inputBorder =
+        OutlineInputBorder(borderSide: Divider.createBorderSide(context));
+    return Container(
+      width: 270,
+      decoration: BoxDecoration(
+        color: Color(0xffffffff),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x19000000),
+            offset: Offset(0, 0),
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: TextField(
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          hintText: hintText,
+          hintStyle: SafeGoogleFont(
+            'Rubik',
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            height: 1.185,
+            letterSpacing: 0.32,
+            color: Color(0x7f000000),
+          ),
+          border: inputBorder,
+          focusedBorder: inputBorder,
+          enabledBorder: inputBorder,
+          filled: true,
+          contentPadding: EdgeInsets.all(8),
+        ),
+        keyboardType: textInputType,
+        controller: controller,
+        obscureText: isPass,
+      ),
+    );
+  }
+}
